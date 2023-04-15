ed9152c2044c18d48cf7f9af1b0e65fcf98e95ad
LeoSuave25 <suaverdez90@gmail.com>
2023-04-15 17:22:06 +0800

    User input and input validation is done

--- vegenerecipher.py
+++ vegenerecipher.py
@@ -1,6 +1,12 @@
 # Style the entire Project
 # Repeat the key until it became the same length as the raw_text
 # Ask User text and save it to variable, validate it to be uppercase letters with no spaces
+while True:
+    plain_text = input("Enter your Text: ")
+    if plain_text.isupper() and ' ' not in plain_text:
+        break
+    else:
+        print("Invalid input! Please enter text in uppercase and with no spaces.")
 # Convert every letters in the text into numbers(0-25)
 # Ask user for the key
 # Convert every letters in the text into numbers(0-25)
