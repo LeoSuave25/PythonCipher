3117d700c14bceeaa2e2493d9a989aaf9d09bb0e
LeoSuave25 <suaverdez90@gmail.com>
2023-04-15 17:13:17 +0800

    Pseudocode is done

---
+++ vegenerecipher.py
@@ -0,0 +1,10 @@
+# Style the entire Project
+# Repeat the key until it became the same length as the raw_text
+# Ask User text and save it to variable, validate it to be uppercase letters with no spaces
+# Convert every letters in the text into numbers(0-25)
+# Ask user for the key
+# Convert every letters in the text into numbers(0-25)
+# Display it in a Row
+# Add two columns
+# Print the Ciphered Text
+# Put the program in loop
